8547ca63d2632717c435e31ed144066b40ebdfb6
Ayushman Gupta <ayushman.gupta308@gmail.com>
2025-10-20 00:05:01 +0530

    Refactor open command to handle empty ID list by retrieving the first article from the database

--- src/commands/open.rs
+++ src/commands/open.rs
@@ -1,14 +1,30 @@
 use anyhow::Result;
 
-use crate::db::{open_connection, queries::find_by_ids};
+use crate::db::{
+    open_connection,
+    queries::{find_by_ids, list_articles},
+};
 
 pub fn execute(ids: &[i64]) -> Result<()> {
     let conn = open_connection()?;
-
+    if ids.is_empty() {
+        let articles = list_articles(&conn, 1, false)?;
+        if let Some(article) = articles.get(0) {
+            browser::that(&article.url)?;
+        } else {
+            println!("No articles found. Add one with `stash add <url>`")
+        }
+        return Ok(());
+    }
     let articles = find_by_ids(&conn, ids)?;
 
-    for article in articles {
-        browser::that(article.url)?;
+    if articles.len() == 0 {
+        println!("No articles found. Add one with `stash add <url>`");
+        return Ok(());
+    }
+
+    for article in &articles {
+        browser::that(&article.url)?;
     }
     Ok(())
 }
